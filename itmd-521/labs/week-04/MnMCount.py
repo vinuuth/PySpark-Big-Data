62814a33709c3a07e2cef567736c997346af1f49
vinuuth <vinuthabp1@gmail.com>
2023-02-06 17:28:25 -0600

    Created mnmcount file

---
+++ itmd-521/labs/week-04/MnMCount.py
@@ -0,0 +1,41 @@
+#Vinutha_Bengaluru_Prabhudev
+import sys
+from pyspark.sql import SparkSession
+
+if __name__ =="_main_":
+    if len(sys/argv) != 2:
+        print("Usage mnmcount <file>", file=sys.stderr)
+        sys.exit(-1)
+
+    spark = (SparkSession.builder.appName.("PythonMnMcount").getOrCreate())
+    mnm_file = sys.argv[1]
+    mnm_df=(spark.read.format("csv")
+            .option("header","true")
+            .option("inferSchema","true")
+            .load(mnm_file))
+
+    count_mnm_df = (mnm_df. select("State","Color","Count")
+    .groupBy("State","Color")
+    .sum("Count")
+    .orderBy("sum(Count)", ascending=False))
+
+    column_mnm_df.show(n=60, truncate=False)
+    print("Total Rows = %d " %(count_mnm_df.count()))
+
+    ca_count_mnm_df = (mnm_df
+        .select("State","Color","Count")
+        .where(mnm_df.state=="CA")
+        .groupBy("State","Color")
+        .sum("Count")
+        .orderBy("sum(Count)", ascending = False))
+
+
+    ca_count-mnm_df.show(n=10, truncate=Falsespark)
+    spark.stop()
+
+
+
+
+
+
+
