dff3d057f71c894cee76988da527516b8d32b297
vinuuth <vinuthabp1@gmail.com>
2023-02-09 10:32:20 -0600

    changes made

--- itmd-521/labs/week-04/src/main/scala/chapter2/MnMcount.scala
+++ itmd-521/labs/week-04/src/main/scala/chapter2/MnMcount.scala
@@ -4,42 +4,45 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
 object MnMcount {
-    def main(args:Array[String]){
-        val spark = SparkSession
-         .builder
-         .appName("MnMCount")
-         .getOrCreate()
+  def main(args: Array[String]) {
+    val spark = SparkSession
+      .builder
+      .appName("MnMCount")
+      .getOrCreate()
 
-        if(args.length < 1)
-        {
-            print("Usage: MnMcount <mnm_file_dataset>")
-            sys.exit(1)
-        }
-
-        val mnmFile = args(0)
-        val mnmDF = spark.read.format("csv")
-         .option("header", "true")
-         .option("inferSchema", "true")
-         .load(mnmFile)
-        mnmDF.show(5, false)
-
-        val countMnMDf = mnmDF.select("State", "Color", "Count")
-            .groupBy("State", "Colour")
-            .sum("Count")
-            .orderBy(desc("sum(Count)"))
-        
-        countMnMDF.show(60)
-        println(s"Total Rows = ${countMnMDF.count()}")
-        println()
-
-        val caCountMnMDF = mnmDF.select("*")
-         .where(col("State") === "CA")
-         .groupBy("State", "Color")
-         .sum("Count")
-         .orderBy(desc("sum(count)"))
-
-        caCountMnMDF.show(10)
-        spark.stop()
-
+    if (args.length < 1) {
+      print("Usage: MnMcount <mnm_file_dataset>")
+      sys.exit(1)
     }
+
+    val mnmFile = args(0)
+    
+    val mnmDF = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load(mnmFile)
+    
+    mnmDF.show(5, false)
+
+    
+    val countMnMDF = mnmDF.select("State", "Color", "Count")
+        .groupBy("State", "Color")
+        .sum("Count")
+        .orderBy(desc("sum(Count)"))
+
+    
+    countMnMDF.show(60)
+    println(s"Total Rows = ${countMnMDF.count()}")
+    println()
+
+    
+    val caCountMnNDF = mnmDF.select("*")
+      .where(col("State") === "CA")
+      .groupBy("State", "Color")
+      .sum("Count")
+      .orderBy(desc("sum(Count)"))
+
+
+    caCountMnNDF.show(10)
+  }
 }
