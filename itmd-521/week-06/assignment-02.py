1375af93a9b0ab336409ef774f7c6c20475ebc27
vinuuth <vinuthabp1@gmail.com>
2023-02-22 12:19:57 -0600

    added structField

--- itmd-521/week-06/assignment-02.py
+++ itmd-521/week-06/assignment-02.py
@@ -1,7 +1,29 @@
+from pyspark.sql import SparkSession
 from pyspark.sql.types import *
+
 from pyspark.sql.functions import *
- 
-sf_fire_file = "/databricks-datasets/learning-spark-v2/sf-fire/sf-fire-calls.csv"
+
+
+if __name__ == "__main__":
+
+    spark = (SparkSession
+        .builder
+        .appName("SF_Fire")
+        .getOrCreate())
+    # get the data set file name
+
+    div = "Divvy_Trips_2015-Q1.csv"
+    sf_fire_file = "/databricks-datasets/learning-spark-v2/sf-fire/sf-fire-calls.csv"
+    # read the file into a Spark DataFrame
+    fire_df = (spark.read.format("csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load(sf_fire_file))
+    fire_df.show()
+
+    print(fire_df.printSchema())
+
+    print("Total rows = %d" % (fire_df.count()))
 
 fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
                      StructField('UnitID', StringType(), True),
@@ -33,13 +55,12 @@ fire_schema = StructType([StructField('CallNumber', IntegerType(), True),
                      StructField('Delay', FloatType(), True)])
 
 
-fire_df = spark.read.csv(sf_fire_file, header=True, schema=fire_schema)
-fire_df.cache()
-fire_df.count()
-fire_df.printSchema()
 
-display(fire_df.limit(50))
-few_fire_df = (fire_df.select("IncidentNumber", "AvailableDtTm", "CallType") 
-              .where(col("CallType") != "Medical Incident"))
+fire_df =spark.read.csv(sf_fire_file, header=True, schema=fire_schema)
 
-few_fire_df.show(5, truncate=False)
+fire_df.show()
+
+print(fire_df.printSchema())
+print(fire_df.schema)
+print ("Total rows = %d" % (fire_df.count()))
+
