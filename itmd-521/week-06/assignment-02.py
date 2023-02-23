746af865e338945af727917e8b758ae25d9f52a3
vinuuth <vinuthabp1@gmail.com>
2023-02-23 03:17:19 -0600

    answer added

--- itmd-521/week-06/assignment-02.py
+++ itmd-521/week-06/assignment-02.py
@@ -124,6 +124,29 @@ max_count_df.select("*").orderBy(desc("count")).show()
 
 fire_san=fire_df2018.select("Neighborhood").where((col("City").like("%San Francisco%")) & (col("CallTypeGroup").like("Fire%"))).groupBy("Neighborhood").agg(count("*").alias("count"))
 fire_san.select("*").orderBy(desc("count")).show()
+# +--------------------+-----+
+# |        Neighborhood|count|
+# +--------------------+-----+
+# |Financial Distric...|   32|
+# |Bayview Hunters P...|   29|
+# |             Mission|   18|
+# |          Tenderloin|   18|
+# |     South of Market|   16|
+# |      Bernal Heights|   12|
+# |     Sunset/Parkside|   12|
+# |        Potrero Hill|   12|
+# |         North Beach|   11|
+# |           Lakeshore|   11|
+# |            Presidio|   10|
+# |           Excelsior|    9|
+# |Oceanview/Merced/...|    8|
+# |      Outer Richmond|    8|
+# |    Western Addition|    7|
+# |         Mission Bay|    7|
+# |    Golden Gate Park|    7|
+# |        Hayes Valley|    6|
+# |       Outer Mission|    6|
+# |        Inner Sunset|    5|
 
 
 
@@ -131,23 +154,62 @@ fire_san.select("*").orderBy(desc("count")).show()
 
 fire_delay=fire_df2018.select("Neighborhood","Delay").where(col("CallTypeGroup").like("Fire%")).orderBy(desc("Delay"))
 fire_delay.show()
+# +--------------------+---------+
+# |        Neighborhood|    Delay|
+# +--------------------+---------+
+# |           Chinatown|491.26666|
+# |Financial Distric...|406.63333|
+# |     Pacific Heights|129.01666|
+# |Bayview Hunters P...|    63.15|
+# |Financial Distric...|    59.35|
+# |       Outer Mission|43.383335|
+# |            Presidio|    38.05|
+# |          Tenderloin|30.566668|
+# |     Treasure Island|24.666666|
+# |      Bernal Heights|24.616667|
+# |Bayview Hunters P...|19.133333|
+# |        Russian Hill|18.616667|
+# |    Golden Gate Park|18.283333|
+# |          Tenderloin|16.333334|
+# |          Tenderloin|14.883333|
+# |     Sunset/Parkside|14.783334|
+# |            Presidio|14.533334|
+# |             Mission|     14.0|
+# | Castro/Upper Market|13.916667|
+# |         North Beach|11.116667|
+# +--------------------+---------+
 
 # Is there a correlation between neighborhood, zip code, and number of fire calls?
 fire_calls_df = fire_df.filter(fire_df.CallTypeGroup.like("Fire%")).groupBy("Neighborhood", "Zipcode").agg(count("*").alias("num_calls"))
 correlation = fire_calls_df.stat.corr("num_calls", "Zipcode")
 fire_calls_df.show()
 print("The correlation between number of fire calls and zip code is:", correlation)
+# +--------------------+-------+---------+
+# |        Neighborhood|Zipcode|num_calls|
+# +--------------------+-------+---------+
+# |        Inner Sunset|  94122|       21|
+# |Bayview Hunters P...|  94124|      299|
+# |        Inner Sunset|  94114|        2|
+# |  West of Twin Peaks|  94112|        8|
+# |           Glen Park|  94110|        1|
+# |        Russian Hill|  94109|       50|
+# |           Excelsior|  94112|       52|
+# |           Chinatown|  94133|       21|
+# |     Pacific Heights|  94115|       39|
+# |    Golden Gate Park|  94117|        6|
+# |        Inner Sunset|  94117|        2|
+# |          Noe Valley|  94131|       16|
+# |    Western Addition|  94117|        6|
+# |      Outer Richmond|  94121|      109|
+# |    Golden Gate Park|  94118|        8|
+# |    Western Addition|  94102|       12|
+# |          Noe Valley|  94110|        2|
+# |          Tenderloin|  94102|      131|
+# |         Mission Bay|  94107|       18|
+# |            Seacliff|  94121|        5|
+# +--------------------+-------+---------+
 
 # Which week in the year in 2018 had the most fire calls?
-#fire_df.select("CallType").where((col("CallType").isNotNull()) & (col("CallDate").like("%2018%")))
-
-#max_count_df = fire_df2018.select(week("IDate").alias("week")).where((col("CallTypeGroup").isNotNull()) & (col("CallTypeGroup").like("Fire%"))).groupBy("week").agg(count("*").alias("count"))
-
-#max_count_df.select("*").orderBy(desc("count")).show()
-
-
-
-
 
 fire_calls_2018 = fire_ts_df.filter(year(fire_ts_df['IDate']) == 2018)
 
@@ -155,7 +217,6 @@ fire_calls_2018 = fire_ts_df.filter(year(fire_ts_df['IDate']) == 2018)
 calls_by_week = fire_calls_2018.groupBy(weekofyear(fire_calls_2018['IDate']).alias("week")).count()
 
 max_calls_week = calls_by_week.orderBy(calls_by_week['count'].desc()).first()['week']
-print(max_calls_week)
 print("Week {} had the most fire calls in 2018 with {} calls.".format(max_calls_week, calls_by_week.filter(calls_by_week['week'] == max_calls_week).first()['count']))
 
 
