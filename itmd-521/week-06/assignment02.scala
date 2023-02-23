0593004415814ac0c5291be62beba9f5af563dc7
vinuuth <vinuthabp1@gmail.com>
2023-02-23 06:33:08 -0600

    corrected infer que 1 code

--- itmd-521/week-06/assignment02.scala
+++ itmd-521/week-06/assignment02.scala
@@ -26,10 +26,10 @@ object assignment02 {
         case class DeviceIoTData (battery_level: Long, c02_level: Long,cca2: String, cca3: String, cn: String, device_id: Long,device_name: String, humidity: Long, ip: String, latitude: Double,lcd: String, longitude: Double, scale:String, temp: Long,timestamp: Long)
 
         // read the file into a Spark DataFrame
-        val iot_df = spark.read.format("json").option("header", "true").option("inferSchema", "true").load(FileJson)
-        println(iot_df.printSchema)
+        val device_df = spark.read.format("json").option("header", "true").option("inferSchema", "true").load(fileJson)
+        println(device_df.printSchema)
     }
 }
   //Detect failing devices with battery levels below a threshold.
-  val threshold= iot_device_df.select("device_name","battery_level").where("battery_level<3")
+  val threshold= device_df.select("device_name","battery_level").where("battery_level<3")
         threshold.show()
