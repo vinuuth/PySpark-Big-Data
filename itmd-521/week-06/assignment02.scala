616aeaeadf9da3befb2a4d1fb90a9d801d69b879
vinuuth <vinuthabp1@gmail.com>
2023-02-23 07:56:25 -0600

    all  ans submitted

--- itmd-521/week-06/assignment02.scala
+++ itmd-521/week-06/assignment02.scala
@@ -21,7 +21,7 @@ object assignment02 {
 
 
   //1)Detect failing devices with battery levels below a threshold.
-  val threshold= device_df.select("device_name","battery_level").where("battery_level<5")
+  val threshold= device_df.select("device_name","battery_level").where("battery_level<3")
         threshold.show()
 
 
