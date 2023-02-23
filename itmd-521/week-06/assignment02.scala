4b291f9a909d2ab648ed103598a90f1e713d2212
vinuuth <vinuthabp1@gmail.com>
2023-02-23 07:03:46 -0600

    co2

--- itmd-521/week-06/assignment02.scala
+++ itmd-521/week-06/assignment02.scala
@@ -23,5 +23,39 @@ object assignment02 {
   //Detect failing devices with battery levels below a threshold.
   val threshold= device_df.select("device_name","battery_level").where("battery_level<5")
         threshold.show()
+
+
+// +--------------------+-------------+
+// |         device_name|battery_level|
+// +--------------------+-------------+
+// | device-mac-36TWSKiT|            2|
+// |sensor-pad-8xUD6p...|            0|
+// |sensor-pad-12Y2kIm0o|            0|
+// |sensor-pad-14QL93...|            1|
+// |meter-gauge-17zb8...|            0|
+// |sensor-pad-36VQv8...|            1|
+// |device-mac-39iklY...|            2|
+// | sensor-pad-40NjeMqS|            2|
+// |meter-gauge-43RYo...|            2|
+// | sensor-pad-448DeWGL|            0|
+// | sensor-pad-52eFObBC|            2|
+// |meter-gauge-77IKW...|            1|
+// |sensor-pad-80TY4d...|            0|
+// |sensor-pad-84jla9J5O|            1|
+// | therm-stick-85NcuaO|            1|
+// |device-mac-87EJxth2l|            1|
+// | sensor-pad-92vxuq7e|            0|
+// |sensor-pad-98mJQA...|            0|
+// |device-mac-99Xh5Y...|            2|
+// |sensor-pad-102D03...|            2|
+// +--------------------+-------------+
+
+
+ //2)Identify offending countries with high levels of CO2 emissions.
+        val co2 = device_df.groupBy("cn").agg(avg("c02_level").alias("av_co2_lvl")).orderBy("av_co2_lvl").sort(desc("av_co2_lvl"))
+        co2.show()
+        
+
+
     }
 }
