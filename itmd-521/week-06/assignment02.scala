deb1461360a88242024d4cb25630195bba9121d7
vinuuth <vinuthabp1@gmail.com>
2023-02-23 07:27:52 -0600

    corrected infer que code

--- itmd-521/week-06/assignment02.scala
+++ itmd-521/week-06/assignment02.scala
@@ -54,7 +54,44 @@ object assignment02 {
  //2)Identify offending countries with high levels of CO2 emissions.
     val emission= device_df.groupBy("cn").agg(avg("c02_level").alias("av_co2_level")).orderBy("av_co2_level").sort(desc("av_co2_level"))
     emission.show()
+
+// +----------------+------------------+
+// |              cn|      av_co2_level|
+// +----------------+------------------+
+// |           Gabon|            1523.0|
+// |Falkland Islands|            1424.0|
+// |          Monaco|            1421.5|
+// |          Kosovo|            1389.0|
+// |      San Marino|1379.6666666666667|
+// |         Liberia|            1374.5|
+// |           Syria|            1345.8|
+// |      Mauritania|1344.4285714285713|
+// |           Congo|          1333.375|
+// |           Tonga|            1323.0|
+// |      East Timor|            1310.0|
+// |          Guinea|            1308.0|
+// |        Botswana|1302.6666666666667|
+// |           Haiti|1291.3333333333333|
+// |            Laos|            1291.0|
+// |        Maldives|1284.7272727272727|
+// |    Sint Maarten|1282.2857142857142|
+// |         Andorra|            1279.0|
+// |         Lesotho|            1274.6|
+// |      Mozambique|            1264.0|
+// +----------------+------------------+
+only showing top 20 rows
         
+ //3)Compute the min and max values for temperature, battery level, CO2, and humidity.
+        val optvalue = iot_df.select(min("temp").alias("min_temperature"),
+        max("temp").alias("max_temperature"),
+        min("battery_level").alias("min_batteryLevel"),
+        max("battery_level").alias("max_batteryLevel"),
+        min("c02_level").alias("min_co2"),
+        max("c02_level").alias("max_co2"),
+        min("humidity").alias("min_humidity"),
+        max("humidity").alias("max_humidity"))
+        optvalue.show()
+
 
 
     }
