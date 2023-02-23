e4ec76957526e55abb1768fee376483a79315b85
vinuuth <vinuthabp1@gmail.com>
2023-02-23 00:36:03 -0600

    sbt

--- itmd-521/week-06/assignment-02.py
+++ itmd-521/week-06/assignment-02.py
@@ -89,20 +89,33 @@ fire_san.show()
 fire_delay=fire_df2018.select("Neighborhood","Delay").where(col("CallTypeGroup").like("Fire%")).orderBy(desc("Delay"))
 fire_delay.show()
 
-# Which week in the year in 2018 had the most fire calls?
-fire_calls_2018_df = fire_ts_df.filter(fire_ts_df.year("IDate") == 2018)
-fire_calls_week_df = fire_calls_2018_df.withColumn("week_number", fire_ts_df.weekofyear("date"))
-fire_calls_count_df = fire_calls_week_df.groupBy("week_number").count()
-fire_calls_count_sorted_df = fire_calls_count_df.sort(fire_ts_df.desc("count"))
-most_fire_calls_week_2018 = fire_calls_count_sorted_df.first()["week_number"]
-most_fire_calls_week_2018.show()
-
-
 # Is there a correlation between neighborhood, zip code, and number of fire calls?
-
 fire_calls_df = fire_df.filter(fire_df.CallTypeGroup.like("Fire%")).groupBy("Neighborhood", "Zipcode").agg(count("*").alias("num_calls"))
 correlation = fire_calls_df.stat.corr("num_calls", "Zipcode")
 fire_calls_df.show()
 print("The correlation between number of fire calls and zip code is:", correlation)
 
+# Which week in the year in 2018 had the most fire calls?
+fire_df.select("CallType").where((col("CallType").isNotNull()) & (col("CallDate").like("%2018%")))
+
+max_count_df = fire_df2018.select(week("IDate").alias("week")).where((col("CallTypeGroup").isNotNull()) & (col("CallTypeGroup").like("Fire%"))).groupBy("week").agg(count("*").alias("count"))
+
+max_count_df.select("*").orderBy(desc("count")).show()
+
+#fire_calls_week_df = fire_calls_2018_df.withColumn("week_number", fire_ts_df.weekofyear("date"))
+#fire_calls_count_df = fire_calls_week_df.groupBy("week_number").count()
+#fire_calls_count_sorted_df = fire_calls_count_df.sort(fire_ts_df.desc("count"))
+#most_fire_calls_week_2018 = fire_calls_count_sorted_df.first()["week_number"]
+#most_fire_calls_week_2018.show()
+
+
 # How can we use Parquet files or SQL tables to store this data and read it back?
+
+parquet_path = ...
+fire_df.write.format("parquet").save(parquet_path)
+
+parquet_table = ... # name of the table
+fire_df.write.format("parquet").saveAsTable(parquet_table)
+
+
+
