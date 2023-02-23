6142737a9d5e07f16897727f9508e4b6f0e71ae5
vinuuth <vinuthabp1@gmail.com>
2023-02-22 19:50:38 -0600

    query2 changes21

--- itmd-521/week-06/assignment-02.py
+++ itmd-521/week-06/assignment-02.py
@@ -72,5 +72,5 @@ fire_ts_df = (fire_df
 
 
 fire_df2018 = fire_ts_df.select(fire_ts_df.columns[:32]).where((col("CallType").isNotNull()) & (col("IDate").like("%2018%"))).show()
-fire_df2018.select(month("IDate")).where((col("CallTypeGroup").isNotNull()) & (col("CallTypeGroup").like("Fire"))).groupBy(month("IDate")).count().show()
-
+#fire_df2018.select(month("IDate")).where((col("CallTypeGroup").isNotNull()) & (col("CallTypeGroup").like("Fire%"))).groupBy(month("IDate")).count().show()
+fire_df2018.select(month("IDate")).show()
