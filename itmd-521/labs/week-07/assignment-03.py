edddfca74e3bbc0de59534904a8d1820ac31f5ce
vinuuth <vinuthabp1@gmail.com>
2023-02-28 10:05:55 -0600

    sql 3

--- itmd-521/labs/week-07/assignment-03.py
+++ itmd-521/labs/week-07/assignment-03.py
@@ -25,12 +25,25 @@ df.createOrReplaceTempView("us_delay_flights_tbl")
 
 
 
-# spark.sql("""SELECT distance, origin, destination
-# FROM us_delay_flights_tbl WHERE distance > 1000
-# ORDER BY distance DESC""").show(10)
+spark.sql("""SELECT distance, origin, destination
+FROM us_delay_flights_tbl WHERE distance > 1000
+ORDER BY distance DESC""").show(10)
 
 
-# spark.sql("""SELECT date, delay, origin, destination
-# FROM us_delay_flights_tbl
-# WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD'
-# ORDER by delay DESC""").show(10)
+spark.sql("""SELECT date, delay, origin, destination
+FROM us_delay_flights_tbl
+WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD'
+ORDER by delay DESC""").show(10)
+
+spark.sql("""SELECT delay, origin, destination,
+ CASE
+ WHEN delay > 360 THEN 'Very Long Delays'
+ WHEN delay > 120 AND delay < 360 THEN 'Long Delays'
+ WHEN delay > 60 AND delay < 120 THEN 'Short Delays'
+ WHEN delay > 0 and delay < 60 THEN 'Tolerable Delays'
+ WHEN delay = 0 THEN 'No Delays'
+ ELSE 'Early'
+ END AS Flight_Delays
+ FROM us_delay_flights_tbl
+ ORDER BY origin, delay DESC""").show(10)
+
