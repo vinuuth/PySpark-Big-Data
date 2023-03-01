7fd2a3b3adab645e06a03a6535e9401ff59d41c6
vinuuth <vinuthabp1@gmail.com>
2023-02-28 22:11:45 -0600

    view resolved with df show()

--- itmd-521/labs/week-07/assignment-03.py
+++ itmd-521/labs/week-07/assignment-03.py
@@ -19,10 +19,7 @@ csv_file = "../../../../LearningSparkV2/databricks-datasets/learning-spark-v2/fl
 # Read and create a temporary view
 # Infer schema (note that for larger files you
 # may want to specify the schema)
-# fy_df = (spark.read.format("csv")
- #.option("inferSchema", "true")
- #.option("header", "true")
- #.load(csv_file))
+
 
 #fly_schema = "date STRING, delay INT, distance INT, origin STRING, destination STRING"
 fly_schema= StructType([StructField('date', StringType(), True),
@@ -33,61 +30,35 @@ fly_schema= StructType([StructField('date', StringType(), True),
                      
 fy_df = spark.read.csv(csv_file, header = True, schema = fly_schema)
 fy_df.show()
-fy_df.createOrReplaceTempView("us_delay_flights_tbl")
+# fy_df.createOrReplaceTempView("us_delay_flights_tbl")
 
 
 
 #Assignment part 1
 
-
-# spark.sql("""SELECT distance, origin, destination
-# FROM us_delay_flights_tbl WHERE distance > 1000
-# ORDER BY distance DESC""").show(10)
-
-
-#from pyspark.sql.functions import col, desc
-(fy_df.select("distance", "origin", "destination")
- .where(col("distance") > 1000)
- .orderBy(desc("distance"))).show(10)
-
-
-# spark.sql("""SELECT date, delay, origin, destination
-# FROM us_delay_flights_tbl
-# WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD'
-# ORDER by delay DESC""").show(10)
-
-#from pyspark.sql.functions import col
-
-
-(fy_df.select("date", "delay", "origin", "destination") \
-    .where((col("delay") > 120) & (col("origin") == "SFO") & (col("destination") == "ORD")) \
-    .orderBy(col("delay").desc()) \
-    .limit(10)).show()
+# (fy_df.select("distance", "origin", "destination")
+#  .where(col("distance") > 1000)
+#  .orderBy(desc("distance"))).show(10)
 
 
 
-# spark.sql("""SELECT delay, origin, destination,
-#  CASE
-#  WHEN delay > 360 THEN 'Very Long Delays'
-#  WHEN delay > 120 AND delay < 360 THEN 'Long Delays'
-#  WHEN delay > 60 AND delay < 120 THEN 'Short Delays'
-#  WHEN delay > 0 and delay < 60 THEN 'Tolerable Delays'
-#  WHEN delay = 0 THEN 'No Delays'
-#  ELSE 'Early'
-#  END AS Flight_Delays
-#  FROM us_delay_flights_tbl
-#  ORDER BY origin, delay DESC""").show(10)
 
-#from pyspark.sql.functions import col, when
+# (fy_df.select("date", "delay", "origin", "destination") \
+#     .where((col("delay") > 120) & (col("origin") == "SFO") & (col("destination") == "ORD")) \
+#     .orderBy(col("delay").desc()) \
+#     .limit(10)).show()
 
-(fy_df.select("delay", "origin", "destination", 
-            when(col("delay") > 360, "Very Long Delays")
-            .when((col("delay") > 120) & (col("delay") < 360), "Long Delays")
-            .when((col("delay") > 60) & (col("delay") < 120), "Short Delays")
-            .when((col("delay") > 0) & (col("delay") < 60), "Tolerable Delays")
-            .when(col("delay") == 0, "No Delays")
-            .otherwise("Early").alias("Flight_Delays")) \
-            .orderBy("origin", col("delay").desc())).show(10)
+
+
+
+# (fy_df.select("delay", "origin", "destination", 
+#             when(col("delay") > 360, "Very Long Delays")
+#             .when((col("delay") > 120) & (col("delay") < 360), "Long Delays")
+#             .when((col("delay") > 60) & (col("delay") < 120), "Short Delays")
+#             .when((col("delay") > 0) & (col("delay") < 60), "Tolerable Delays")
+#             .when(col("delay") == 0, "No Delays")
+#             .otherwise("Early").alias("Flight_Delays")) \
+#             .orderBy("origin", col("delay").desc())).show(10)
 
 #Assignment part 2
 # From page 90-92, you will create a Table named us_delay_flights_tbl from the departuredelay.csv
@@ -100,14 +71,14 @@ fy_df.createOrReplaceTempView("us_delay_flights_tbl")
 
 fy_df = fy_df.withColumn("dateMonth", from_unixtime(unix_timestamp(fy_df.date, "MMddHHmm"), "MM")).withColumn("dateDay", from_unixtime(unix_timestamp(fy_df.date, "MMddHHmm"), "dd"))
 print("with column completed")
-
+fy_df.show(1)
 # csv_file = "../../../../LearningSparkV2/databricks-datasets/learning-spark-v2/flights/departuredelays.csv"
 # Schema as defined in the preceding example
 
-fy_df.write.saveAsTable("us_delay_flights_tbl")
+fy_df.write.saveAsTable("us_delay_flights_tbl_1")
 print("save completed")
 # query= "SELECT dateMonth, dateDay, delay, origin, destination FROM us_delay_flights_tbl WHERE origin ='ORD' AND dateMonth = 3 AND dateDay >= 1 AND dateDay <= 15 ORDER BY delay DESC LIMIT 5;"
-query = "SELECT * FROM us_delay_flights_tbl"
+query = "SELECT * FROM us_delay_flights_tbl_1"
 sol_query_df= spark.sql(query)
 print("query creation completed")
 sol_query_df.createOrReplaceTempView("us_delay_flights_tbl_tmp_view")
