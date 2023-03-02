19a79d7a9e77c9fb78c16de487e57003fcaed6d6
vinuuth <vinuthabp1@gmail.com>
2023-03-02 05:02:50 -0600

    scala query2

--- itmd-521/labs/week-07/src/main/scala/chapter4/assignment-03.scala
+++ itmd-521/labs/week-07/src/main/scala/chapter4/assignment-03.scala
@@ -41,15 +41,19 @@ fli_df.select("date", "delay", "origin", "destination")
  .show(10)
 
 //For the third query
-fli_df.select("delay", "origin", "destination",
-when(col("delay") > 360, "Very Long Delays")
-.when((col("delay") > 120) && (col("delay") < 360), "Long Delays")
-.when((col("delay") > 60) && (col("delay") < 120), "Short Delays")
-.when((col("delay") > 0) && (col("delay") < 60), "Tolerable Delays")
-.when(col("delay") === 0, "No Delays")
-.otherwise("Early")
-.alias("Flight_Delays"))
-.orderBy("origin", col("delay").desc())
+fli_df.select(
+  col("delay"), 
+  col("origin"), 
+  col("destination"),
+  when(col("delay") > 360, "Very Long Delays")
+    .when((col("delay") > 120) && (col("delay") < 360), "Long Delays")
+    .when((col("delay") > 60) && (col("delay") < 120), "Short Delays")
+    .when((col("delay") > 0) && (col("delay") < 60), "Tolerable Delays")
+    .when(col("delay") === 0, "No Delays")
+    .otherwise("Early")
+    .alias("Flight_Delays")
+)
+.orderBy(col("origin"), col("delay").desc())
 .show(10)
 
     }
