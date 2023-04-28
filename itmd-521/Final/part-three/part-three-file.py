91075e2fc03e45078c31adb49747d425dd7bbf35
vinuuth <vinuthabp1@gmail.com>
2023-04-27 21:49:52 -0500

    part 3 query 3run

--- itmd-521/Final/part-three/part-three-file.py
+++ itmd-521/Final/part-three/part-three-file.py
@@ -23,7 +23,7 @@ conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 conf.set("fs.s3a.connection.ssl.enabled", "false")
 
 # Create SparkSession Object - tell the cluster the FQDN of the host system)
-spark = SparkSession.builder.appName("VIN-read-50-parquet-secondrun").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
+spark = SparkSession.builder.appName("VIN-read-50-parquet-thirdrun").config('spark.driver.host','spark-edge-vm0.service.consul').config(conf=conf).getOrCreate()
 
 
 parquet_file = "s3a://vbengaluruprabhudev/50-parquet"
